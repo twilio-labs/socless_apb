0d96d0660ad5840fc80f9d380e9f762f7749ae37
Saxon Hunt <shunt@twilio.com>
2021-09-15 16:22:37 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,9 +18,9 @@ export const DECORATOR_FLAGS = Object.freeze({
 });
 
 // Here we explicitly assign `string` to prevent `const` type incompatibility with Record<string, State>
-export const PLAYBOOK_FORMATTER_STEP_NAME = "PLAYBOOK_FORMATTER" as string;
-export const PLAYBOOK_DIRECT_INVOCATION_CHECK_STEP_NAME = "Was_Playbook_Direct_Executed" as string;
-export const PLAYBOOK_SETUP_STEP_NAME = "Setup_Socless_Global_State" as string;
+export const PLAYBOOK_FORMATTER_STEP_NAME: string = "PLAYBOOK_FORMATTER";
+export const PLAYBOOK_DIRECT_INVOCATION_CHECK_STEP_NAME: string = "Was_Playbook_Direct_Executed";
+export const PLAYBOOK_SETUP_STEP_NAME: string = "Setup_Socless_Global_State";
 
 export const SOCLESS_CORE_LAMBDA_NAME_FOR_RUNNING_PLAYBOOK_SETUP =
   "_socless_setup_global_state_for_direct_invoked_playbook";
