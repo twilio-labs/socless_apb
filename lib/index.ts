dcc2652618782cfa8dcf97aea990ae7256923c9a
Saxon Hunt <shunt@twilio.com>
2021-10-14 22:53:57 -0700

    update public api exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,8 @@
 export * from "./types";
 export { apb } from "./apb";
-export { PARSE_SELF_NAME } from "./constants";
+export {
+  PARSE_SELF_NAME,
+  STATES_EXECUTION_ROLE_ARN,
+  AWS_EVENT_RULE_RESOURCE_TYPE,
+} from "./constants";
 export { ValidationResult, validateState } from "./validators";
