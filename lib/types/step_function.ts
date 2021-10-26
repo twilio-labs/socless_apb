bb581478ebf1904bbac67811264c187f53c81131
Saxon Hunt <shunt@twilio.com>
2021-10-25 23:39:46 -0700

    add Catch & Retry types

--- lib/types/step_function.ts
+++ lib/types/step_function.ts
@@ -2,27 +2,27 @@ import { State } from ".";
 
 // Customized from https://states-language.net/
 
-// shared attributes for all END or CHOICE states (Succeed, Fail, Choice)
+/** shared attributes for all END or CHOICE states (Succeed, Fail, Choice) */
 export interface BaseStateTerminal {
   Type: string;
   Comment?: string;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-succeed-state.html
+/**  https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-succeed-state.html */
 export interface SucceedState extends BaseStateTerminal {
   Type: "Succeed";
   InputPath?: string;
   OutputPath?: string;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-fail-state.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-fail-state.html  */
 export interface FailState extends BaseStateTerminal {
   Type: "Fail";
   Cause?: string; // Provides a custom failure string that can be used for operational or diagnostic purposes.
   Error?: string; // Provides an error name that can be used for operational or diagnostic purposes.
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-choice-state.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-choice-state.html  */
 export interface ChoiceState extends BaseStateTerminal {
   Type: "Choice";
   Choices: ChoiceRule[];
@@ -31,8 +31,8 @@ export interface ChoiceState extends BaseStateTerminal {
   OutputPath?: string;
 }
 
-// Shared attributes for all regular States (Wait, Pass) & (Task, Parallel, Map, Interaction)
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-common-fields.html
+/** Shared attributes for all regular States (Wait, Pass) & (Task, Parallel, Map, Interaction)
+/* https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-common-fields.html */
 export interface CommonStateFields {
   Type: string;
   Comment?: string;
@@ -43,7 +43,7 @@ export interface CommonStateFields {
   End?: boolean;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-wait-state.html
+/**  https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-wait-state.html */
 export interface WaitState extends CommonStateFields {
   Type: "Wait";
   // Only one of the following -
@@ -53,7 +53,7 @@ export interface WaitState extends CommonStateFields {
   TimestampPath?: string;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-pass-state.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-pass-state.html  */
 export interface PassState extends CommonStateFields {
   Type: "Pass";
   Parameters?: Record<string, unknown>;
@@ -61,16 +61,16 @@ export interface PassState extends CommonStateFields {
   ResultPath?: string;
 }
 
-// shared attributes for all States that do actions (Task, Parallel, Map, Interaction)
+/** shared attributes for all States that do actions (Task, Parallel, Map, Interaction) */
 export interface BaseStateAction extends CommonStateFields {
   Parameters?: Record<string, unknown>;
   ResultPath?: string;
   ResultSelector?: Record<string, unknown>;
   Retry?: unknown[];
-  Catch?: unknown[];
+  Catch?: Catch[];
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-task-state.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-task-state.html  */
 export interface TaskState extends BaseStateAction {
   Type: "Task";
   Resource: string;
@@ -80,7 +80,7 @@ export interface TaskState extends BaseStateAction {
   HeartbeatSecondsPath?: string;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-map-state.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-map-state.html */
 export interface MapState extends BaseStateAction {
   Type: "Map";
   Iterator: StepFunction;
@@ -88,13 +88,27 @@ export interface MapState extends BaseStateAction {
   MaxConcurrency: number;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-parallel-state.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-parallel-state.html */
 export interface ParallelState extends BaseStateAction {
   Type: "Parallel";
   Branches: StepFunction[];
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-choice-state.html#amazon-states-language-choice-state-rules
+export interface Catch {
+  ErrorEquals: string[]; // must have at least 1 string.
+  Next: string;
+  ResultPath?: String;
+}
+
+/**https://docs.aws.amazon.com/step-functions/latest/dg/concepts-error-handling.html#error-handling-retrying-after-an-error */
+export interface Retry {
+  ErrorEquals: string[]; // must have at least 1 string.
+  IntervalSeconds: number;
+  MaxAttempts: number;
+  BackoffRate: number;
+}
+
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-choice-state.html#amazon-states-language-choice-state-rules  */
 export interface ChoiceRule {
   Variable: string;
   Next?: string;
@@ -142,7 +156,7 @@ export interface ChoiceRule {
   IsTimestamp?: boolean;
 }
 
-// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-state-machine-structure.html
+/** https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-state-machine-structure.html  */
 export interface StepFunction {
   StartAt: string;
   States: Record<string, State>;
@@ -151,7 +165,7 @@ export interface StepFunction {
   Version?: string;
 }
 
-// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-stepfunctions-statemachine.html
+/** https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-stepfunctions-statemachine.html  */
 export interface CloudFormationStateMachine {
   Type: "AWS::StepFunctions::StateMachine";
   Properties: CloudFormationStateMachineProperties;
